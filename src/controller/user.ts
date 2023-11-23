0b1a39cc4e3d4655205d1a07bfecb84c585f3980
alibarmonster <alibar838@gmail.com>
2023-11-23 18:17:42 +0700

    chore: add try and catch

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -4,36 +4,48 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const getUserById = async (req: Request, res: Response) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const foundUser = await prisma.user.findFirst({
-    where: {
-      id: id,
-    },
-  });
-
-  if (!foundUser) {
-    return res.status(404).json({
-      message: 'id not found',
+    const foundUser = await prisma.user.findFirst({
+      where: {
+        id: id,
+      },
     });
-  }
 
-  return res.status(200).json({
-    message: 'user found',
-    data: foundUser,
-  });
+    if (!foundUser) {
+      return res.status(404).json({
+        message: 'id not found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'user found',
+      data: foundUser,
+    });
+  } catch (error) {
+    return res.json(error);
+  }
+};
+
+const updateUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
 };
 
 const getAllUser = async (req: Request, res: Response) => {
-  const allUser = await prisma.user.findMany();
+  try {
+    const allUser = await prisma.user.findMany();
 
-  return res.status(200).json({
-    message: 'success get All User',
-    data: {
-      allUser,
-      total: allUser.length,
-    },
-  });
+    return res.status(200).json({
+      message: 'success get All User',
+      data: {
+        allUser,
+        total: allUser.length,
+      },
+    });
+  } catch (error) {
+    return res.json(error);
+  }
 };
 
 export { getUserById, getAllUser };
