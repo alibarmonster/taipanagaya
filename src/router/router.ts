98a3be4063ee3a12e79b042463c970a41c0679fa
alibarmonster <alibar838@gmail.com>
2023-11-23 18:10:45 +0700

    style: change routing style

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,18 +1,27 @@
-import express, { Router } from 'express';
-import { register, login } from '../controller/auth';
+import express, { Response, Router } from 'express';
+import * as authController from '../controller/auth';
 import validateRegister from '../middleware/validateRegister';
 import { LoginSchema, RegisterSchema } from '../Schema/userSchema';
 import validateLogin from '../middleware/ValidateLogin';
 import { verifyJwt } from '../middleware/verifyJwt';
+import * as userController from '../controller/user';
+import * as postController from '../controller/post';
+import { verifyRoles } from '../middleware/verifyRoles';
+import { Role } from '../utils';
 
 const router: Router = express.Router();
 
-router.post('/register', validateRegister(RegisterSchema), register);
-router.post('/login', validateLogin(LoginSchema), login);
-router.get('/users', verifyJwt, async (req: express.Request, res: express.Response) => {
-  return res.status(200).json({
-    message: 'congrats u passed the authorization with jwt token',
-  });
+//auth router
+router.post('/register', validateRegister(RegisterSchema), authController.register);
+router.post('/login', validateLogin(LoginSchema), authController.login);
+
+//user router
+router.get('/users/:id', userController.getUserById);
+router.get('/users', userController.getAllUser);
+router.get('/alibar', verifyJwt, verifyRoles(Role.ADMIN), async (req, res: Response) => {
+  return res.json('only admin can access this resource');
 });
 
+// Posts router
+router.post('/posts', verifyJwt, postController.createPost);
 export default router;
